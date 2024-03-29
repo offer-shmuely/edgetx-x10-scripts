c32cd9c4330c9bf40f9c665cdb4f099a1c4a025c
shmuely <offer.shmuely@gmail.com>
2024-03-29 10:13:28 +0300

    FrSky_SR10_plus

---
+++ SCRIPTS/TOOLS/FrSky_SR10_plus_calibrate.lua
@@ -0,0 +1,8 @@
+-- TNS|FrSky SR10+ calibrate|TNE
+
+local function run()
+    return "/SCRIPTS/TOOLS/FrSky_SR10_plus/calibrate.lua"
+end
+
+return { run = run }
+
---
+++ SCRIPTS/TOOLS/FrSky_SR10_plus_setup.lua
@@ -0,0 +1,8 @@
+-- TNS|FrSky SR10+ Setup|TNE
+
+local function run()
+    return "/SCRIPTS/TOOLS/FrSky_SR10_plus/setup.lua"
+end
+
+return { run = run }
+
