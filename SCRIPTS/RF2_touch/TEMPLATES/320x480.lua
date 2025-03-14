73f51da900d8f6c5ec185771c5d85a59a051f1ae
shmuely <offer.shmuely@gmail.com>
2025-03-14 23:16:07 +0200

    feat!: rf2_touch is moving to separeate repo

--- SCRIPTS/RF2_touch/TEMPLATES/320x480.lua
+++
@@ -1,7 +0,0 @@
-return {
-    margin       = 5,
-    indent       = 15,
-    lineSpacing  = 20,
-    listSpacing  = { line = 20, field = 170 },
-    tableSpacing = { row = 25, col = 60, header = 20 },
-}
--- SCRIPTS/TOOLS/RF2_touch.lua
+++
@@ -1,8 +0,0 @@
-local toolName = "TNS|RF2 Touch|TNE"
-
-local function run()
-    return "/SCRIPTS/RF2_touch/tool.lua"
-end
-
-return { run = run }
-
