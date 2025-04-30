8157e4fe7b78ec2349196356be0e8d476ea6cb82
shmuely <offer.shmuely@gmail.com>
2025-04-30 17:00:30 +0300

    feat: add Frsky_Gyro_suite (remove old scripts)

--- SCRIPTS/TOOLS/FrSky_SR10_plus_calibrate.lua
+++
@@ -1,8 +0,0 @@
--- TNS|FrSky SR10+ calibrate|TNE
-
-local function run()
-    return "/SCRIPTS/TOOLS/FrSky_SR10_plus/calibrate.lua"
-end
-
-return { run = run }
-
--- SCRIPTS/TOOLS/FrSky_SR10_plus_setup.lua
+++
@@ -1,8 +0,0 @@
--- TNS|FrSky SR10+ Setup|TNE
-
-local function run()
-    return "/SCRIPTS/TOOLS/FrSky_SR10_plus/setup.lua"
-end
-
-return { run = run }
-
